3b41d2ae6ac76e747b88fddbe4755add5982a8b6
Victor Silva <victorgabriel08fs@gmail.com>
2020-11-24 16:29:25 -0300

    v1.3

--- netflix/src/components/Header/index.js
+++ netflix/src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import './styles.css';
+import logo from '../../assets/logo.svg';
+import face from '../../assets/face.png';
 
 const Header = ({ black }) => {
     return (
@@ -8,12 +10,12 @@ const Header = ({ black }) => {
         <header className={black ? 'black' : ''}>
             <div className="header--logo" >
                 <a href="/">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix" />
+                    <img src={logo} alt="Netflix" />
                 </a>
             </div>
             <div className="header--user" >
                 <a href="/">
-                    <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Usuário" />
+                    <img src={face} alt="Usuário" />
                 </a>
             </div>
         </header>
