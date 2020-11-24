90f07e5da674a7324bdf3ed3b2182a51902a53ba
Victor Silva <victorgabriel08fs@gmail.com>
2020-11-24 16:26:47 -0300

    1.3

--- netflix/src/components/Header/index.js
+++ netflix/src/components/Header/index.js
@@ -8,7 +8,7 @@ const Header = ({ black }) => {
         <header className={black ? 'black' : ''}>
             <div className="header--logo" >
                 <a href="/">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="Netflix" />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix" />
                 </a>
             </div>
             <div className="header--user" >
